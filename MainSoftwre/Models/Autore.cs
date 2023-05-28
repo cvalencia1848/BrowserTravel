cfe99c65b1a0bd2423d2674c327e94080a38b184
Cristian Camilo Valencia <cristian.valencia@xpertgroup.co>
2023-05-28 12:08:52 -0500

    Add project files.

---
+++ MainSoftwre/Models/Autore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace MainSoftwre.Models
+{
+    public partial class Autore
+    {
+        public Autore()
+        {
+            AutoresHasLibros = new HashSet<AutoresHasLibro>();
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+
+        [NotMapped]
+        public string NombreCompleto { get { return $"{Nombre} {Apellidos}"; } }
+
+        public virtual ICollection<AutoresHasLibro> AutoresHasLibros { get; set; }
+    }
+}
---
+++ MainSoftwre/Models/AutoresHasLibro.cs
@@ -0,0 +1,17 @@
+﻿#nullable disable
+
+using System.ComponentModel.DataAnnotations;
+
+namespace MainSoftwre.Models
+{
+    public partial class AutoresHasLibro
+    {
+        public int Id { get; set; }
+        public int AutoresId { get; set; }
+        public int LibrosIsbn { get; set; }
+
+        public virtual Autore Autores { get; set; }
+        [Display(Name = "Libros")]
+        public virtual Libro LibrosIsbnNavigation { get; set; }
+    }
+}
